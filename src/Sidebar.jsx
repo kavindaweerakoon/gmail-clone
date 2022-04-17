148041899de55f472b28b27a7778ee27a8deac89
Kavinda Weerakoon <kw@kwnetic.com>
2022-04-16 20:12:38 -0400

    cleanup 4/16/2022

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,47 +1,65 @@
 import React from "react";
 import { Button } from "@mui/material";
-import InboxIcon from '@mui/icons-material/Inbox';
+import InboxIcon from "@mui/icons-material/Inbox";
 import "./Sidebar.css";
 import SidebarOption from "./SidebarOption";
-import StarIcon from '@mui/icons-material/Star';
-import { AccessTime, Duo, ExpandMore, LabelImportant, NearMe, Note, Person, Phone } from "@mui/icons-material";
+import StarIcon from "@mui/icons-material/Star";
+import {
+  AccessTime,
+  Duo,
+  ExpandMore,
+  LabelImportant,
+  NearMe,
+  Note,
+  Person,
+  Phone,
+} from "@mui/icons-material";
 import { IconButton } from "@mui/material";
-import { useDispatch } from 'react-redux'
-import { openSendMessage } from './features/mailSlice'
+import { useDispatch } from "react-redux";
+import { openSendMessage } from "./features/mailSlice";
+import { useNavigate } from "react-router-dom";
 
 function Sidebar() {
-
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const emailCount = localStorage.getItem("emailCount") || 0;
+  function handleClick() {
+    navigate(`/`);
+  }
+
   return (
     <div className="sidebar">
-      <Button className="sidebar__compose" 
-      onClick={() => dispatch(openSendMessage())} >
+      <Button
+        className="sidebar__compose"
+        onClick={() => dispatch(openSendMessage())}
+      >
         <img
           src="https://www.gstatic.com/images/icons/material/colored_icons/1x/create_32dp.png"
           alt="compose"
         />
         Compose
       </Button>
-      <SidebarOption Icon={InboxIcon} title="Inbox" number={emailCount} />
-      <SidebarOption Icon={StarIcon} title="Starred"  />
-      <SidebarOption Icon={AccessTime} title="Snoozed"  />
-      <SidebarOption Icon={LabelImportant} title="Important"  />
-      <SidebarOption Icon={NearMe} title="Sent"  />
-      <SidebarOption Icon={Note} title="Drafts"  />
-      <SidebarOption Icon={ExpandMore} title="More"  />
+      <div className="inbox"  onClick={handleClick}>
+        <SidebarOption Icon={InboxIcon} title="Inbox" number={emailCount} />
+      </div>
+      <SidebarOption Icon={StarIcon} title="Starred" />
+      <SidebarOption Icon={AccessTime} title="Snoozed" />
+      <SidebarOption Icon={LabelImportant} title="Important" />
+      <SidebarOption Icon={NearMe} title="Sent" />
+      <SidebarOption Icon={Note} title="Drafts" />
+      <SidebarOption Icon={ExpandMore} title="More" />
 
       <div className="sidebar__footer">
         <div className="sidebar__footerIcons">
-            <IconButton>
-                <Person/>
-            </IconButton>
-            <IconButton>
-                <Duo/>
-            </IconButton>
-            <IconButton>
-                <Phone/>
-            </IconButton>
+          <IconButton>
+            <Person />
+          </IconButton>
+          <IconButton>
+            <Duo />
+          </IconButton>
+          <IconButton>
+            <Phone />
+          </IconButton>
         </div>
       </div>
     </div>
